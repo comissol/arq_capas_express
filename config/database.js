55bdd61b4829fb54f55dd0dbe7aa8e6abe33b184
Lucila Comisso <comissol92@gmail.com>
2026-07-18 13:00:01 -0300

    correccion safety

--- config/database.js
+++ config/database.js
@@ -3,9 +3,7 @@ process.loadEnvFile();
 
 const { DB_PROTOCOL, DB_HOST, DB_PASS, DB_USER, DB_OPTIONS, DB_NAME } =
   process.env;
-const MONGODB_URI = 'mongodb://comissol92_db_user:ieuB5mNIA5TAiLpm@ac-ble6lqd-shard-00-00.ffbq0mi.mongodb.net:27017,ac-ble6lqd-shard-00-01.ffbq0mi.mongodb.net:27017,ac-ble6lqd-shard-00-02.ffbq0mi.mongodb.net:27017/dogsDB?ssl=true&replicaSet=atlas-9zcxv9-shard-0&authSource=admin&appName=Comissol'
-
-//`${DB_PROTOCOL}://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?${DB_OPTIONS}`;
+const MONGODB_URI = `${DB_PROTOCOL}://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?${DB_OPTIONS}`;
 
 const connectDB = async () => {
   try {
